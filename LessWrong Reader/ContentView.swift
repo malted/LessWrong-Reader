45f438d44d1d522642bfde679bde9a4ad91dd109
Malted <malted@malted.dev>
2024-06-22 09:04:41 -0400

    Initial Commit

---
+++ LessWrong Reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LessWrong Reader
+//
+//  Created by Ben Dixon on 6/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LessWrong Reader/LessWrong_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessWrong_ReaderApp.swift
+//  LessWrong Reader
+//
+//  Created by Ben Dixon on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LessWrong_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
